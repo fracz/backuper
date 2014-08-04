fead8c5ec782553ea573856d9c9fe3f83e228cfb
fracz <fraczwojciech@gmail.com>
2014-08-04 23:11:14 +0200

    Initial version

---
+++ backup.php
@@ -0,0 +1,66 @@
+<?php
+
+error_reporting(E_ERROR);
+
+if(!file_exists('config.php'))
+    die('config.php file does not exists. Use config.sample.php to create your configuration.');
+
+$dir = __DIR__;
+
+require $dir . '/config.php';
+require_once $dir . '/vendor/autoload.php';
+
+
+$fileName = date('Y.m.d_H.i');
+$dirPath = $dir;
+$localFilePath = "$dirPath/$fileName";
+
+$cmd = sprintf("%s -u %s --password=\"%s\" -d %s --skip-no-data > %s",
+    MYSQLDUMP,
+    escapeshellcmd(DB_USERNAME),
+    escapeshellcmd(DB_PASSWORD),
+    escapeshellcmd(DB_DATABASE),
+    escapeshellcmd($localFilePath . '.sql'));
+
+exec($cmd);
+
+exec('cd ' . $dirPath . ' && tar czf ' . $fileName . '.tgz ' . $fileName . '.sql');
+
+unlink($localFilePath . '.sql');
+
+$localFilePath .= '.tgz';
+
+$copy = new \Barracuda\Copy\API(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, SECRET);
+$fh = fopen($localFilePath, 'rb');
+$parts = [];
+while ($data = fread($fh, 1024 * 1024)) {
+    $parts[] = $copy->sendData($data);
+}
+fclose($fh);
+$copy->createFile($copyConfig->backup_dir . $filename, $parts);
+
+$backups = $copy->listPath($copyConfig->backup_dir);
+foreach($backups as $backup){
+    if($oldFilesFrom > $backup->created_time){
+        $copy->removeFile($backup->path);
+    }
+}
+
+if(LOCAL_DIR){
+    rename($localFilePath, LOCAL_DIR . '/' . $fileName . '.tgz');
+    $dir = opendir(LOCAL_DIR);
+    $oldFilesFrom = time() - BACKUP_EXPIRATION;
+    while ($file = readdir($dir)) {
+        if (end(explode('.', $file)) == 'tgz') {
+            $time = filemtime($dirPath . '/' . $file);
+            if ($time < $oldFilesFrom) {
+                unlink($dirPath . '/' . $file);
+            }
+        }
+    }
+} else {
+    unlink($localFilePath);
+}
+
+
+
---
+++ config.sample.php
@@ -0,0 +1,15 @@
+<?php
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('TOKEN', '');
+define('SECRET', '');
+
+define('MYSQLDUMP', 'mysqldump');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+
+// set to false if don't want to store local files
+define('LOCAL_DIR', __DIR__ . '/backups');
+define('COPY_DIR', '/backup/');
+define('BACKUP_EXPIRATION', 86400 * 3); // 3 days
